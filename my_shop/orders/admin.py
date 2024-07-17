f54c90e9c3426e60e58d3262cc17660df454f9b4
Marat Shaibakov <smaspb17@yandex.ru>
2024-07-18 00:19:33 +0300

    Added action in admin panel to export orders to csv file

--- my_shop/orders/admin.py
+++ my_shop/orders/admin.py
@@ -1,4 +1,8 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 
 from orders.models import Order, OrderItem
@@ -9,21 +13,50 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
-def order_stripe_payment(obj):
-    url = obj.get_stripe_url()
-    if obj.stripe_id:
-        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
-        return mark_safe(html)
-    return ''
+def export_to_csv(self, request, queryset):
+    opts = self.model._meta
+    print(dir(opts))
+    content_disposition = f'attachment; filename={opts.object_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if
+              not field.many_to_many and not field.one_to_many]
+    # записать первую строку с именами полей
+    writer.writerow([field.verbose_name for field in fields])
+    # записать строки данных
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
 
 
-order_stripe_payment.short_description = 'ПЛАТЕЖ STRIPE'
+export_to_csv.short_description = 'Экспорт в CSV'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid', order_stripe_payment,
+                    'postal_code', 'city', 'paid', 'order_stripe_payment',
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
+
+    @admin.display(description='Платеж Stripe')
+    def order_stripe_payment(self, obj):
+        url = obj.get_stripe_url()
+        if obj.stripe_id:
+            html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+            return mark_safe(html)
+        return ''
+
+    # order_stripe_payment.short_description = 'Платеж Stripe'
+
+
+
